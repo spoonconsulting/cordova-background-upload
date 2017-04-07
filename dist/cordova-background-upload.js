0b69673af34fee38d2a1075b31da66d79c6c3d9f
mevinDhun <mevin.dhunnooa@sc-mauritius.com>
2017-04-07 11:07:15 +0400

    added write to file on disk for mobile

--- dist/cordova-background-upload.js
+++ dist/cordova-background-upload.js
@@ -80,17 +80,35 @@ return /******/ (function(modules) { // webpackBootstrap
 	        var w = window;
 	        if (w.cordova) {
 	            //on mobile device
-	            if (!payload.filePath) {
-	                return errorCb("filePath parameter is required");
+	            if (typeof FileTransferManager == 'undefined') {
+	                return errorCb('cordova-plugin-background-upload not found..');
 	            }
-	            if (payload.filePath == "") {
-	                return errorCb("invalid filePath");
+	            var fileObject = payload.file;
+	            var directoryPath = cordova.file.cacheDirectory;
+	            console.log(directoryPath);
+	            if (!directoryPath) {
+	                return errorCb('cordova-plugin-file not found..install it via: cordova plugin add cordova-plugin-file --save');
 	            }
-	            if (typeof FileTransferManager != 'undefined') {
-	                //use cordova plugin https://github.com/spoonconsulting/cordova-plugin-background-upload
-	                return new FileTransferManager().upload(payload).then(successCb, errorCb, progressCb);
-	            }
-	            return errorCb('cordova-plugin-background-upload not found..');
+	            window.resolveLocalFileSystemURL(directoryPath, function (dir) {
+	                dir.getFile(fileObject.name, {
+	                    create: true
+	                }, function (tempFile) {
+	                    tempFile.createWriter(function (fileWriter) {
+	                        fileWriter.write(fileObject);
+	                        payload.filePath = directoryPath + fileObject.name;
+	                        if (!payload.filePath) {
+	                            return errorCb("filePath parameter is required");
+	                        }
+	                        if (payload.filePath == "") {
+	                            return errorCb("invalid filePath");
+	                        }
+	                        //use cordova plugin https://github.com/spoonconsulting/cordova-plugin-background-upload
+	                        return new FileTransferManager().upload(payload).then(successCb, errorCb, progressCb);
+	                    }, function (e) {
+	                        errorCb("error writing file to disk for upload: " + e);
+	                    });
+	                });
+	            });
 	        }
 	        if (!payload.file) {
 	            return errorCb("file parameter is required");
--- src/BackgroundUpload.ts
+++ src/BackgroundUpload.ts
@@ -1,4 +1,5 @@
 declare var FileTransferManager: any;
+declare var cordova: any;
 
 //https://github.com/TypeStrong/ts-loader#loading-other-resources-and-code-splitting
 declare var require: { <
@@ -12,8 +13,7 @@ var request: any = require('superagent/lib/client');
 
 export class BackgroundUpload {
 
-  constructor() {
-  }
+  constructor() {}
 
   upload(payload, successCb, errorCb, progressCb) {
 
@@ -32,23 +32,43 @@ export class BackgroundUpload {
     var w: any = window;
     if (w.cordova) {
       //on mobile device
-      
-      if (!payload.filePath) {
-          return errorCb("filePath parameter is required");
-        }
 
-        if (payload.filePath == "") {
-          return errorCb("invalid filePath");
-        }
-      
-       if (typeof FileTransferManager != 'undefined') {
-         //use cordova plugin https://github.com/spoonconsulting/cordova-plugin-background-upload
-        return new FileTransferManager().upload(payload).then(successCb, errorCb, progressCb);
+      if (typeof FileTransferManager == 'undefined') {
+        return errorCb('cordova-plugin-background-upload not found..');
+      }
 
-      } 
-        
-      return errorCb('cordova-plugin-background-upload not found..');
-    
+      var fileObject = payload.file;
+      var directoryPath = cordova.file.cacheDirectory;
+      console.log(directoryPath);
+      if (!directoryPath){
+        return errorCb('cordova-plugin-file not found..install it via: cordova plugin add cordova-plugin-file --save');
+      }
+      window.resolveLocalFileSystemURL(directoryPath, function (dir) {
+        ( < DirectoryEntry > dir).getFile(fileObject.name, {
+          create: true
+        }, function (tempFile) {
+          tempFile.createWriter(function (fileWriter) {
+            fileWriter.write(fileObject);
+            payload.filePath = directoryPath + fileObject.name;
+
+            if (!payload.filePath) {
+              return errorCb("filePath parameter is required");
+            }
+
+            if (payload.filePath == "") {
+              return errorCb("invalid filePath");
+            }
+
+            //use cordova plugin https://github.com/spoonconsulting/cordova-plugin-background-upload
+            return new FileTransferManager().upload(payload).then(successCb, errorCb, progressCb);
+
+          }, function (e) {
+            errorCb("error writing file to disk for upload: " + e);
+          });
+        });
+      });
+
+
     }
 
     if (!payload.file) {
